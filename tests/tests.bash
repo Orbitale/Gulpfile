4d8f0bd97be8fb52ab88ba7bbd892314feacbb47
Alex Rock Ancelet <pierstoval@gmail.com>
2016-06-10 18:00:56 +0200

    Update test script to fix directory bug

--- tests/tests.bash
+++ tests/tests.bash
@@ -10,7 +10,7 @@ echo "Installing dependencies..."
 npm install
 
 echo "Preparing tests..."
-node prepare_tests.js
+node tests/prepare_tests.js
 
 echo "Executing gulp dump..."
-./node_modules/.bin/gulp --gulpfile gulpfile.js dump
+./node_modules/.bin/gulp --gulpfile tests/gulpfile.js dump
